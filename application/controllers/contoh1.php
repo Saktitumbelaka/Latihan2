70c81e767109e8ed5c6707c8d8a2504523ecc77b
Saktitumbelaka <saktitumbwlaka@gmail.com>
2021-09-25 01:30:23 +0700

    commit pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Trisakti Tumbelaka<br>
+            saya tinggal di daerah kota Bekasi<br>
+            Hobi saya bermain sepakbola seperti Arby karena Arby seperti Messi";
+    }
+}
